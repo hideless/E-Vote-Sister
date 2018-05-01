5902acd9f8d95ea70faae8628229a0574f930da9
Hendy Sujarwo <hendyway@gmail.com>
2018-05-01 17:17:57 +0700

    FINAL CANDIDATE

--- candidate.py
+++ candidate.py
@@ -4,27 +4,31 @@ class candidate():
     def cekAdmin(self, username, password):
         s = xmlrpc.client.ServerProxy('http://192.168.0.5:5000')
         cekAdmin = s.dataAdmin(username, password)
+        time = str(s.getTime())
 
-        if (cekAdmin == True):
-            bem  = (s.candidateAppBEM())
-            dpm = (s.candidateAppDPM())
-            hima = (s.candidateAppHIMA())
-            print("\n", "===== HASIL PEMILIHAN E-VOTE =====", "\n")
+        if (time >= "2018-05-01 16:51:07"):
+            if (cekAdmin == True):
+                bem  = (s.candidateAppBEM())
+                dpm = (s.candidateAppDPM())
+                hima = (s.candidateAppHIMA())
+                print("\n", "===== HASIL PEMILIHAN E-VOTE =====", "\n")
 
-            print("------ HASIL BEM ------")
-            for i in range(len(bem)):
-                print(bem[i])
-            print("\n","------ HASIL DPM ------")
-            for j in range(len(dpm)):
-                print(dpm[j])
-            print("\n","------ HASIL HIMA ------")
-            for k in range(len(hima)):
-                print(hima[k])
+                print("------ HASIL BEM ------")
+                for i in range(len(bem)):
+                    print(bem[i])
+                print("\n","------ HASIL DPM ------")
+                for j in range(len(dpm)):
+                    print(dpm[j])
+                print("\n","------ HASIL HIMA ------")
+                for k in range(len(hima)):
+                    print(hima[k])
 
-            print("\n ================================== \n")
+                print("\n ================================== \n")
+            else:
+                print("Login Gagal!")
+                print("Username atau Password salah!")
         else:
-            print("Login Gagal!")
-            print("Username atau Password salah!")
+            print("Voting masih berjalan")
 while True:
     print("----- MENU CANDIDATE -----")
     username = input("Username : ")
